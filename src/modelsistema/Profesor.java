2731e2a781ab7fc95b1986fc69deb54aff017bc8
turkovic98 <eminaturkovic600@gmail.com>
2020-01-02 18:31:01 +0100

    #profesor

---
+++ src/modelsistema/Profesor.java
@@ -0,0 +1,106 @@
+package modelsistema;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+public class Profesor {
+
+	private String ime;
+	private String prezime;
+	private Date datumRodjenja;
+	private String adresaStanovanja;
+	private int kontaktTelefon;
+	private String email;
+	private String adresaKancelarije;
+	private int brojLicneKarte;
+	private String  Titula;
+	private String Zvanje;
+	private ArrayList<Predmet> spisakPredmetaNaKojimaPredaje;
+
+	public Profesor() {
+		super();
+	}
+	public Profesor(String ime, String prezime, Date datumRodjenja, String adresaStanovanja, int kontaktTelefon, String email,
+			String adresaKancelarije, int brojLicneKarte, String titula,String zvanje, ArrayList<Predmet> spisak) {
+		super();
+		this.ime=ime;
+		this.prezime=prezime;
+		this.datumRodjenja=datumRodjenja;
+		this.adresaStanovanja=adresaStanovanja;
+		this.kontaktTelefon=kontaktTelefon;
+		this.email=email;
+		this.adresaKancelarije=adresaKancelarije;
+		this.brojLicneKarte=brojLicneKarte;
+		this.Titula=titula;
+		this.Zvanje=zvanje;
+		this.spisakPredmetaNaKojimaPredaje=spisak;
+	}
+	public String getIme() {
+		return ime;
+	}
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	public String getPrezime() {
+		return prezime;
+	}
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+	public Date getDatumRodjenja() {
+		return datumRodjenja;
+	}
+	public void setDatumRodjenja(Date datumRodjenja) {
+		this.datumRodjenja = datumRodjenja;
+	}
+	public String getAdresaStanovanja() {
+		return adresaStanovanja;
+	}
+	public void setAdresaStanovanja(String adresaStanovanja) {
+		this.adresaStanovanja = adresaStanovanja;
+	}
+	public int getKontaktTelefon() {
+		return kontaktTelefon;
+	}
+	public void setKontaktTelefon(int kontaktTelefon) {
+		this.kontaktTelefon = kontaktTelefon;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAdresaKancelarije() {
+		return adresaKancelarije;
+	}
+	public void setAdresaKancelarije(String adresaKancelarije) {
+		this.adresaKancelarije = adresaKancelarije;
+	}
+	public int getBrojLicneKarte() {
+		return brojLicneKarte;
+	}
+	public void setBrojLicneKarte(int brojLicneKarte) {
+		this.brojLicneKarte = brojLicneKarte;
+	}
+	public String getTitula() {
+		return Titula;
+	}
+	public void setTitula(String titula) {
+		Titula = titula;
+	}
+	public String getZvanje() {
+		return Zvanje;
+	}
+	public void setZvanje(String zvanje) {
+		Zvanje = zvanje;
+	}
+	public ArrayList<Predmet> getSpisakPredmetaNaKojimaPredaje() {
+		return spisakPredmetaNaKojimaPredaje;
+	}
+	public void setSpisakPredmetaNaKojimaPredaje(
+			ArrayList<Predmet> spisakPredmetaNaKojimaPredaje) {
+		this.spisakPredmetaNaKojimaPredaje = spisakPredmetaNaKojimaPredaje;
+	}
+	
+}
