636c9d4d328107316e712f25eb0ec5761271f2ab
turkovic98 <eminaturkovic600@gmail.com>
2020-01-04 14:15:50 +0100

    #ProfesoriController

---
+++ src/controllers/ProfesoriController.java
@@ -0,0 +1,41 @@
+package controllers;
+
+import java.sql.Date;
+
+import izgledaplikacije.AbstractTableModelProfesori;
+import izgledaplikacije.ProfesoriJTable;
+import modelsistema.BazaProfesora;
+import modelsistema.Titula;
+import modelsistema.Zvanje;
+
+public class ProfesoriController {
+	private static ProfesoriController instance=null;
+	
+	public static ProfesoriController getInstance() {
+		if(instance==null) {
+			instance=new ProfesoriController(); 
+		}
+		return instance;
+	}
+	
+	public void izbrisiProfesora(int brojLicneKarte) {
+		BazaProfesora.getInstance().izbrisiProfesora(brojLicneKarte);
+		
+		AbstractTableModelProfesori model=(AbstractTableModelProfesori) ProfesoriJTable.getInstance().getModel();
+		model.fireTableDataChanged();
+	}
+	
+	public void dodajProfesora(String ime, String prezime, Date datumRodjenja, String adresaStanovanja, int kontaktTelefon, String email,
+			String adresaKancelarije, int brojLicneKarte, Titula titula,Zvanje zvanje) {
+		BazaProfesora.getInstance().dodajProfesora(ime, prezime, datumRodjenja, adresaStanovanja, kontaktTelefon, email, adresaKancelarije, brojLicneKarte, titula, zvanje);
+		
+		AbstractTableModelProfesori model=(AbstractTableModelProfesori) ProfesoriJTable.getInstance().getModel();
+		model.fireTableDataChanged();
+	}
+	/*
+	public void promenaPosleDeserijalizacije() {
+		AbstractTableModelProfesori model=(AbstractTableModelProfesori) ProfesoriJTable.getInstance().getModel();
+		model.fireTableDataChanged();
+	}*/
+
+}
