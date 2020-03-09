892c6e4e6c20800faa757a5d4910fedc88cf430d
Ana00000 <anaatanackovic98@gmail.com>
2020-03-09 12:35:01 +0100

    #izmena

--- src/izgledaplikacije/MyApp.java
+++ src/izgledaplikacije/MyApp.java
@@ -1,9 +1,7 @@
 package izgledaplikacije;
 
 public class MyApp {
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
 		MainFrame mf = new MainFrame();
 	    mf.setVisible(true); // Prozor je inicijalno nevidljiv
